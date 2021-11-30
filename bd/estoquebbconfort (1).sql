3f0faa07ad65b78a9d615d92ed0e1173d263c51e
MMacedoS <macedo.mauriciogen@gmail.com>
2021-11-30 07:30:29 -0300

    bd

--- bd/estoquebbconfort (1).sql
+++ bd/estoquebbconfort (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Abr-2021 às 14:09
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.3
+-- Tempo de geração: 30-Nov-2021 às 11:29
+-- Versão do servidor: 10.4.18-MariaDB
+-- versão do PHP: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `estoque`
+-- Banco de dados: `estoquebbconfort`
 --
+CREATE DATABASE IF NOT EXISTS `estoquebbconfort` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `estoquebbconfort`;
 
 DELIMITER $$
 --
 -- Procedimentos
 --
+DROP PROCEDURE IF EXISTS `Atualiza_estoque`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Atualiza_estoque` (IN `id_produto` INT, IN `quantidade` INT)  NO SQL
 BEGIN
 declare contador int(11);
@@ -44,33 +46,10 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `Vendas`
---
-
-CREATE TABLE `Vendas` (
-  `id_apt` int(11) NOT NULL,
-  `nome` varchar(45) NOT NULL,
-  `status` int(11) NOT NULL,
-  `caixa` int(11) DEFAULT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `Vendas`
---
-
-INSERT INTO `Vendas` (`id_apt`, `nome`, `status`, `caixa`, `createdAt`) VALUES
-(1, 'APT 01', 1, 15, '2021-04-11 13:40:25'),
-(2, 'APT 02', 0, 12, '2021-04-11 13:40:25'),
-(3, 'APT 03', 0, NULL, '2021-04-11 13:40:25'),
-(4, 'APT 04', 0, NULL, '2021-04-11 13:40:25');
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `caixa`
 --
 
+DROP TABLE IF EXISTS `caixa`;
 CREATE TABLE `caixa` (
   `id_caixa` int(11) NOT NULL,
   `caixa` varchar(45) DEFAULT NULL,
@@ -85,23 +64,16 @@ CREATE TABLE `caixa` (
 --
 
 INSERT INTO `caixa` (`id_caixa`, `caixa`, `abertura`, `fechamento`, `status`, `createdAt`) VALUES
-(4, 'APT 01', '2021-03-09', NULL, 1, '2021-04-11 13:39:50'),
-(8, 'APT 02', '2021-03-10', NULL, 1, '2021-04-11 13:39:50'),
-(9, 'APT 02', '2021-03-10', NULL, 1, '2021-04-11 13:39:50'),
-(10, 'APT 01', '2021-03-10', NULL, 1, '2021-04-11 13:39:50'),
-(11, 'APT 02', '2021-03-10', NULL, 1, '2021-04-11 13:39:50'),
-(12, 'APT 02', '2021-03-19', '2021-04-11', 1, '2021-04-11 13:39:50'),
-(13, 'APT 01', '2021-03-22', '2021-03-30', 1, '2021-04-11 13:39:50'),
-(14, 'APT 01', '2021-04-11', '2021-04-13', 1, '2021-04-11 13:39:50'),
-(15, 'APT 01', '2021-04-13', NULL, 0, '2021-04-13 11:58:58');
+(30, '17', '2021-11-30', NULL, 0, '2021-11-30 09:24:24');
 
 --
 -- Acionadores `caixa`
 --
+DROP TRIGGER IF EXISTS `Trigger_caixa_update`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_caixa_update` AFTER UPDATE ON `caixa` FOR EACH ROW BEGIN
 IF (new.status<>0) THEN
-UPDATE Vendas SET status=0 where caixa=new.id_caixa; 
+UPDATE vendas SET status=0 where caixa=new.id_caixa; 
 END IF;
 END
 $$
@@ -113,6 +85,7 @@ DELIMITER ;
 -- Estrutura da tabela `categoria`
 --
 
+DROP TABLE IF EXISTS `categoria`;
 CREATE TABLE `categoria` (
   `id_cat` int(11) NOT NULL,
   `categoria` varchar(45) NOT NULL,
@@ -126,20 +99,34 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id_cat`, `categoria`, `status`, `imagem`, `createdAt`) VALUES
-(1, 'teste', 'true', 'a04110cfebdabdf1a646836a49e3b177.png', '2021-04-11 13:40:57'),
-(2, 'teste', 'true', '44b2217c7a427325f2ca2f175580f375.png', '2021-04-11 13:40:57'),
-(3, 'Bebidas', 'true', '55fa6d68d6e22c1e1e7561c4c78925f9.png', '2021-04-11 13:40:57'),
-(4, 'drinks', 'true', '0087bb618e6408228aa2edaf5b4c6960.png', '2021-04-11 13:40:57'),
-(5, 'Salgados', 'true', '561cdadf88f53f9b7baa283e9a4c3d2b.jpg', '2021-04-11 13:40:57'),
-(6, 'Cervejas', 'true', '529f952237e721591a314c50b6eca218.png', '2021-04-11 13:40:57'),
-(7, 'Refrigerante', 'true', '7df446fefcf3e21aa9102c464c65335b.png', '2021-04-11 13:40:57'),
-(8, 'Refrigerante lata', 'true', 'bd6c2ae905b0dddf36c1e00abb5dc212.png', '2021-04-11 13:40:57'),
-(9, 'Cerveja Litrinho', 'true', '7a7ba68796ed7e7838374dcc2d5f3216.png', '2021-04-11 13:40:57'),
-(10, 'Petiscos', 'true', 'a827e166c6e607f160fef94e726ce639.png', '2021-04-11 13:40:57'),
-(11, 'Bolos', 'true', '', '2021-04-11 13:40:57'),
-(12, 'Doces', 'true', 'c03d3dcb0f145396e74680913c197a2b.png', '2021-04-11 13:40:57'),
-(13, 'testeaas', 'true', '058485d07d584eab703df85ccdd4debd.png', '2021-04-11 13:40:57'),
-(14, 'pizza', 'true', '1bb7a6fbb1703b7f48364e3b15dc85a3.jpg', '2021-04-11 13:40:57');
+(16, 'BICO 01', 'true', '4e8be590d398dfb1f980789d18d06559.png', '2021-11-30 09:14:27'),
+(17, 'BICO 02', 'true', '51c06f02e358fcf031091d4f13b40098.png', '2021-11-30 09:14:51'),
+(18, 'BICO 03', 'true', 'ff2b2017c25c1180e7fe677ee6221e2a.png', '2021-11-30 09:15:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `endereco` text NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
+  `credito` decimal(7,2) NOT NULL,
+  `created_At` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nome`, `endereco`, `telefone`, `email`, `status`, `credito`, `created_At`) VALUES
+(4, 'Mauricio ', 'centro', '(75) 99287-2929', 'macedo.mauricio@gmail.com', 1, '0.00', '2021-11-30 06:13:19');
 
 -- --------------------------------------------------------
 
@@ -147,6 +134,7 @@ INSERT INTO `categoria` (`id_cat`, `categoria`, `status`, `imagem`, `createdAt`)
 -- Estrutura da tabela `consumo`
 --
 
+DROP TABLE IF EXISTS `consumo`;
 CREATE TABLE `consumo` (
   `id_consumo` int(11) NOT NULL,
   `id_caixa` int(11) NOT NULL,
@@ -162,41 +150,22 @@ CREATE TABLE `consumo` (
 --
 
 INSERT INTO `consumo` (`id_consumo`, `id_caixa`, `valor`, `quantidade`, `data`, `produto`, `createdAt`) VALUES
-(4, 4, '20.00', 1, '2021-03-09', 1, '2021-04-11 10:42:21'),
-(6, 4, '20.00', 1, '2021-03-09', 2, '2021-04-11 10:42:21'),
-(7, 4, '20.00', 10, '2021-03-09', 1, '2021-04-11 10:42:21'),
-(8, 8, '20.00', 20, '2021-03-10', 1, '2021-04-11 10:42:21'),
-(10, 8, '5.50', 1, '2021-03-10', 2, '2021-04-11 10:42:21'),
-(11, 8, '34.00', 2, '2021-03-10', 3, '2021-04-11 10:42:21'),
-(12, 8, '5.00', 1, '2021-03-10', 2, '2021-04-11 10:42:21'),
-(16, 11, '4.50', 1, '2021-03-10', 2, '2021-04-11 10:42:21'),
-(17, 11, '5.00', 1, '2021-03-10', 1, '2021-04-11 10:42:21'),
-(18, 11, '34.00', 2, '2021-03-10', 3, '2021-04-11 10:42:21'),
-(20, 10, '5.00', 1, '2021-03-11', 1, '2021-04-11 10:42:21'),
-(22, 10, '5.00', 1, '2021-03-11', 1, '2021-04-11 10:42:21'),
-(23, 10, '5.00', 1, '2021-03-14', 1, '2021-04-11 10:42:21'),
-(28, 12, '5.00', 1, '2021-03-19', 1, '2021-04-11 10:42:21'),
-(29, 12, '4.50', 1, '2021-03-19', 2, '2021-04-11 10:42:21'),
-(30, 12, '17.00', 1, '2021-03-19', 3, '2021-04-11 10:42:21'),
-(31, 13, '5.00', 1, '2021-03-22', 4, '2021-04-11 10:42:21'),
-(32, 13, '5.00', 1, '2021-03-30', 4, '2021-04-11 10:42:21'),
-(33, 13, '4.50', 1, '2021-03-30', 2, '2021-04-11 10:42:21'),
-(34, 13, '5.00', 1, '2021-03-30', 1, '2021-04-11 10:42:21'),
-(35, 14, '4.50', 1, '2021-04-11', 2, '2021-04-11 10:42:21'),
-(36, 14, '50.00', 10, '2021-04-13', 4, '2021-04-13 08:58:03'),
-(37, 15, '5.00', 1, '2021-04-13', 4, '2021-04-13 08:59:05');
+(117, 30, '116.00', 20, '2021-11-30', 6, '2021-11-30 07:05:17');
 
 --
 -- Acionadores `consumo`
 --
+DROP TRIGGER IF EXISTS `Trigger_consumo_delete`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_consumo_delete` AFTER DELETE ON `consumo` FOR EACH ROW CALL Atualiza_estoque(old.produto,old.quantidade)
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `Trigger_cosumo_insert`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_cosumo_insert` AFTER INSERT ON `consumo` FOR EACH ROW CALL Atualiza_estoque(new.produto,new.quantidade * (-1))
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `Trigger_cosumo_update`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_cosumo_update` AFTER UPDATE ON `consumo` FOR EACH ROW CALL Atualiza_estoque(new.produto,old.quantidade - new.quantidade)
 $$
@@ -208,6 +177,7 @@ DELIMITER ;
 -- Estrutura da tabela `entrada_estoque`
 --
 
+DROP TABLE IF EXISTS `entrada_estoque`;
 CREATE TABLE `entrada_estoque` (
   `id_entra` int(11) NOT NULL,
   `id_prod` int(11) NOT NULL,
@@ -221,27 +191,22 @@ CREATE TABLE `entrada_estoque` (
 --
 
 INSERT INTO `entrada_estoque` (`id_entra`, `id_prod`, `data`, `quantidade`, `createdAt`) VALUES
-(1, 4, '2021-03-14', 100, '2021-04-11 10:43:13'),
-(2, 2, '2021-03-09', 10, '2021-04-11 10:43:13'),
-(4, 2, '2021-03-10', 100, '2021-04-11 10:43:13'),
-(5, 3, '2021-03-10', 10, '2021-04-11 10:43:13'),
-(6, 1, '2021-03-10', 20, '2021-04-11 10:43:13'),
-(7, 3, '2021-03-10', 10, '2021-04-11 10:43:13'),
-(8, 2, '2021-03-10', 15, '2021-04-11 10:43:13'),
-(9, 2, '2021-03-10', 15, '2021-04-11 10:43:13'),
-(10, 4, '2021-03-19', 200, '2021-04-11 10:43:13');
+(13, 9, '2021-11-30', 1000, '2021-11-30 07:14:10');
 
 --
 -- Acionadores `entrada_estoque`
 --
+DROP TRIGGER IF EXISTS `Trigger_delete`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_delete` AFTER DELETE ON `entrada_estoque` FOR EACH ROW CALL Atualiza_estoque(old.id_prod,old.quantidade)
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `Trigger_insert`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_insert` AFTER INSERT ON `entrada_estoque` FOR EACH ROW CALL Atualiza_estoque(new.id_prod,new.quantidade)
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `Trigger_update`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_update` AFTER UPDATE ON `entrada_estoque` FOR EACH ROW CALL Atualiza_estoque(new.id_prod,new.quantidade-old.quantidade)
 $$
@@ -253,6 +218,7 @@ DELIMITER ;
 -- Estrutura da tabela `estoque`
 --
 
+DROP TABLE IF EXISTS `estoque`;
 CREATE TABLE `estoque` (
   `id_est` int(11) NOT NULL,
   `id_prod` int(11) NOT NULL,
@@ -265,10 +231,48 @@ CREATE TABLE `estoque` (
 --
 
 INSERT INTO `estoque` (`id_est`, `id_prod`, `saldo_Atual`, `saldo_Anterior`) VALUES
-(1, 1, 191, 192),
-(2, 2, 134, 135),
-(3, 3, 13, 14),
-(4, 4, 92, 93);
+(6, 6, 180, 200),
+(7, 7, 200, 0),
+(8, 8, 200, 0),
+(9, 9, 1200, 200);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historicocategoria`
+--
+
+DROP TABLE IF EXISTS `historicocategoria`;
+CREATE TABLE `historicocategoria` (
+  `codigo` int(11) NOT NULL,
+  `Fechamento` int(11) NOT NULL,
+  `codigoCategoria` int(11) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento`
+--
+
+DROP TABLE IF EXISTS `pagamento`;
+CREATE TABLE `pagamento` (
+  `id_pagamento` int(11) NOT NULL,
+  `valor` decimal(7,2) NOT NULL,
+  `tipo` varchar(40) NOT NULL,
+  `status` int(11) NOT NULL,
+  `id_caixa` int(11) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pagamento`
+--
+
+INSERT INTO `pagamento` (`id_pagamento`, `valor`, `tipo`, `status`, `id_caixa`, `created_at`) VALUES
+(82, '100.00', 'Carteira', 0, 30, '2021-11-30'),
+(83, '16.00', 'Carteira', 0, 30, '2021-11-30');
 
 -- --------------------------------------------------------
 
@@ -276,6 +280,7 @@ INSERT INTO `estoque` (`id_est`, `id_prod`, `saldo_Atual`, `saldo_Anterior`) VAL
 -- Estrutura da tabela `produto`
 --
 
+DROP TABLE IF EXISTS `produto`;
 CREATE TABLE `produto` (
   `id_prod` int(11) NOT NULL,
   `descricao` varchar(45) NOT NULL,
@@ -294,14 +299,15 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`id_prod`, `descricao`, `fornecedor`, `status`, `precoVenda`, `precoCompra`, `quantidade`, `categoria`, `imagem`, `createdAt`) VALUES
-(1, 'Coca-Cola', 'Coca-Cola', 'true', '5.00', '2.50', 3, 8, 'f4b627c3b101a6ebf61376abb14e0a22.png', '2021-04-11 10:43:58'),
-(2, 'Fanta', 'Coca-Cola', 'true', '4.50', '2.50', 5, 8, '31007d0827415e3a8bdaaa7b7e54536f.png', '2021-04-11 10:43:58'),
-(3, 'Petiscos', 'eua', 'true', '17.00', '15.00', 1, 10, 'c918df695b0c08974acf3f61b2a24ab4.png', '2021-04-11 10:43:58'),
-(4, 'Cerveja Litrinho', 'Skol', 'true', '5.00', '0.00', 5, 9, '8e2eecffe857b16a2aadcae5c5d46def.png', '2021-04-11 10:43:58');
+(6, 'Gasolina Comum', 'Brasil', 'true', '5.80', '4.30', 200, 16, '464ee2a61d7d36b61c64265eaf297be3.png', '2021-11-30 06:16:18'),
+(7, 'Gasolina ADITIVADA', 'Brasil', 'true', '5.80', '4.30', 200, 17, 'bd4a3c7ec9cceb0e14f2713a7ba59d80.png', '2021-11-30 06:16:34'),
+(8, 'Alcool', 'Brasil', 'true', '5.80', '4.30', 200, 18, '656c78b640fd1d3047584bba0a1ad0c5.png', '2021-11-30 06:16:45'),
+(9, 'Gasolina Comum', 'Brasil', 'true', '5.00', '4.00', 200, 16, '1f92c79ffcab485c8fd3493825032c9e.png', '2021-11-30 06:17:14');
 
 --
 -- Acionadores `produto`
 --
+DROP TRIGGER IF EXISTS `Trigger_produto_insert`;
 DELIMITER $$
 CREATE TRIGGER `Trigger_produto_insert` AFTER INSERT ON `produto` FOR EACH ROW CALL Atualiza_estoque(new.id_prod,new.quantidade)
 $$
@@ -310,9 +316,27 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `saida`
+--
+
+DROP TABLE IF EXISTS `saida`;
+CREATE TABLE `saida` (
+  `id_saida` int(11) NOT NULL,
+  `descricao` varchar(60) NOT NULL,
+  `valor` int(11) NOT NULL,
+  `data` date NOT NULL DEFAULT current_timestamp(),
+  `observacao` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `saida_estoque`
 --
 
+DROP TABLE IF EXISTS `saida_estoque`;
 CREATE TABLE `saida_estoque` (
   `id_saida` int(11) NOT NULL,
   `cliente` varchar(100) NOT NULL,
@@ -328,6 +352,7 @@ CREATE TABLE `saida_estoque` (
 -- Estrutura da tabela `usuarios`
 --
 
+DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `id_user` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -344,17 +369,35 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id_user`, `email`, `nome`, `senha`, `acesso`, `createdAt`) VALUES
 (1, 'macedo@hotmail.com', 'Mauricio Macedo', 'ed3831389108737760f657d1087b8569', 'admin', '2021-04-11 10:44:31'),
 (4, 'macedos@hotmail.com', 'Mauricio Macedo', '202cb962ac59075b964b07152d234b70', 'admin', '2021-04-11 10:44:31'),
-(5, 'mauricio@hotmail.com', 'mauricio ', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', '2021-04-11 10:44:31');
+(5, 'mauricio@hotmail.com', 'mauricio ', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', '2021-04-11 10:44:31'),
+(6, 'macedo@pitstop', 'mauricio', '202cb962ac59075b964b07152d234b70', 'admin', '2021-11-28 09:59:32');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendas`
+--
+
+DROP TABLE IF EXISTS `vendas`;
+CREATE TABLE `vendas` (
+  `id_venda` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `caixa` int(11) DEFAULT NULL,
+  `cliente` int(11) NOT NULL DEFAULT 1,
+  `createdAt` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `vendas`
+--
+
+INSERT INTO `vendas` (`id_venda`, `nome`, `status`, `caixa`, `cliente`, `createdAt`) VALUES
+(17, 'Caixa 01', 1, 30, 4, '2021-11-30');
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices para tabela `Vendas`
---
-ALTER TABLE `Vendas`
-  ADD PRIMARY KEY (`id_apt`);
 
 --
 -- Índices para tabela `caixa`
@@ -367,6 +410,12 @@ ALTER TABLE `caixa`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_cat`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
 
 --
 -- Índices para tabela `consumo`
@@ -389,10 +438,28 @@ ALTER TABLE `estoque`
   ADD PRIMARY KEY (`id_est`);
 
 --
+-- Índices para tabela `historicocategoria`
+--
+ALTER TABLE `historicocategoria`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices para tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  ADD PRIMARY KEY (`id_pagamento`);
+
+--
 -- Índices para tabela `produto`
 --
 ALTER TABLE `produto`
   ADD PRIMARY KEY (`id_prod`);
+
+--
+-- Índices para tabela `saida`
+--
+ALTER TABLE `saida`
+  ADD PRIMARY KEY (`id_saida`);
 
 --
 -- Índices para tabela `saida_estoque`
@@ -407,50 +474,74 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- Índices para tabela `vendas`
 --
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`id_venda`);
 
 --
--- AUTO_INCREMENT de tabela `Vendas`
+-- AUTO_INCREMENT de tabelas despejadas
 --
-ALTER TABLE `Vendas`
-  MODIFY `id_apt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `caixa`
 --
 ALTER TABLE `caixa`
-  MODIFY `id_caixa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_caixa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `consumo`
 --
 ALTER TABLE `consumo`
-  MODIFY `id_consumo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_consumo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT de tabela `entrada_estoque`
 --
 ALTER TABLE `entrada_estoque`
-  MODIFY `id_entra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_entra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `estoque`
 --
 ALTER TABLE `estoque`
-  MODIFY `id_est` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_est` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `historicocategoria`
+--
+ALTER TABLE `historicocategoria`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  MODIFY `id_pagamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `saida`
+--
+ALTER TABLE `saida`
+  MODIFY `id_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `saida_estoque`
@@ -462,7 +553,13 @@ ALTER TABLE `saida_estoque`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restrições para despejos de tabelas
